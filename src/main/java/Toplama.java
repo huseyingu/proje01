de551353464701830607ba299cedf3d62fb20bce
Huseyin Guzel <hguzel6065@gmail.com>
2023-03-12 20:39:57 -0400

    v2

--- src/main/java/Toplama.java
+++ src/main/java/Toplama.java
@@ -2,7 +2,12 @@ public class Toplama {
     public static void main(String[] args) {
         topl(3,2);
     }
-    public static  void  topl(int a, int b){
-        System.out.println(a+b);
+    public static  void  topl(int... a){
+         int toplam=0;
+        for (int w:a){
+            toplam+=w;
+        }
+
+        System.out.println("toplam ="+toplam);
     }
 }
